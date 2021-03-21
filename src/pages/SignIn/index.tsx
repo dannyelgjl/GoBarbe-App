362035b5ca63c992a7573fbe5c9d657206f749be
dannyelgjl <dannyelgjl@gmail.com>
2021-03-21 10:58:48 -0300

    feat: config images

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,16 @@
 import React from "react";
-
+// Components React-Native
+import { Image } from "react-native";
+// logo
+import logoImg from "../../assets/logo.png";
+// styles
 import { Container } from "./styles";
 
 const SignIn: React.FC = () => {
   return (
-    <Container />
+    <Container>
+      <Image source={logoImg} />
+    </Container>
   );
 }
 
