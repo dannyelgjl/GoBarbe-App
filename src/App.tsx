9e2d5bbb88139ae8c3c576889d6c590a6caa14fc
dannyelgjl <dannyelgjl@gmail.com>
2021-03-21 10:47:31 -0300

    feat: configuring navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import 'react-native-gesture-handler';
 import React from "react";
+// Components React-Native
 import { View, StatusBar } from "react-native";
+// Navigation
+import { NavigationContainer } from '@react-navigation/native';
+// Routes
+import Routes from "./routes";
 
 const App: React.FC = () => (
-  <>
+  <NavigationContainer>
     <StatusBar barStyle="light-content" backgroundColor="#312e38" />
-    <View style={{ flex: 1, backgroundColor: '#312e38' }} />
-  </>
+    <View style={{ flex: 1, backgroundColor: '#312e38' }} >
+      <Routes />
+    </View>
+  </NavigationContainer>
 );
 
 
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { View } from "react-native";
+import { Container } from "./styles";
 
 const SignUp: React.FC = () => {
   return (
-    <View />
+    <Container />
   );
 }
 
